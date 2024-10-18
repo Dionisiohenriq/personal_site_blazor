cf626c6eb3c60c204e92475b78941b044d2703be
dionisiohenriq <henriquedionisio.dionisioferre@gmail.com>
2024-10-18 17:42:41 -0300

    first commit

---
+++ PersonalSite/Models/Home.cs
@@ -0,0 +1,8 @@
+namespace PersonalSite.Models;
+
+public class Home
+{
+    public string? Title { get; set; } = "Test";
+    public string? Content { get; set; } = "Test Content";
+    public bool? MyInfo { get; set; } = false;
+}
